87c012c7815fff7908ae0092b16d1d287e37dae7
MManolov32 <124664916+MManolov32@users.noreply.github.com>
2025-11-29 21:25:07 +0200

    Add files via upload

---
+++ MagicGreeting.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+
+        string greeting = MagicGreeting(name);
+        Console.WriteLine(greeting);
+    }
+
+    static string MagicGreeting(string name)
+    {
+        string[] messages =
+        {
+            $"Welcome, mighty {name}!",
+            $"Greetings, traveler {name}!",
+            $"Behold, the legendary {name} has arrived!",
+            $"Ah, it's you again {name}!",
+            $"{name}, prepare for greatness!"
+        };
+
+        Random rnd = new Random();
+        int pick = rnd.Next(messages.Length);
+
+        return messages[pick];
+    }
+}
