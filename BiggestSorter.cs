68a27d96c38b630be90f77e0e5840f22e6297316
MManolov32 <124664916+MManolov32@users.noreply.github.com>
2025-11-29 21:20:01 +0200

    Add files via upload

---
+++ BiggestSorter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] nums = { 5, 1, 7, 3, 9, 2 };
+
+        SortDescending(nums);
+
+        Console.WriteLine("Sorted descending:");
+        foreach (int n in nums)
+            Console.WriteLine(n);
+    }
+
+    static int FindBiggest(int[] arr)
+    {
+        int biggest = arr[0];
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] > biggest)
+            {
+                biggest = arr[i];
+            }
+        }
+        return biggest;
+    }
+
+    static void SortDescending(int[] arr)
+    {
+        // really basic sorting (swap biggest to front)
+        for (int i = 0; i < arr.Length; i++)
+        {
+            for (int j = i + 1; j < arr.Length; j++)
+            {
+                if (arr[j] > arr[i])
+                {
+                    int tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+        }
+    }
+}
